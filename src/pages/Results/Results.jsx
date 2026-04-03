2d551749a5c21f76fdad94af66ad0b7a413e4661
eva <evacaruanac@gmail.com>
2026-04-03 09:55:35 +0200

    added email routine features

--- src/pages/Results/Results.jsx
+++ src/pages/Results/Results.jsx
@@ -5,6 +5,8 @@ import { mapProducts } from "../../utils/mapProducts";
 import { useLocation, useNavigate } from "react-router-dom";
 import { generateRoutine } from "../../utils/generateRoutine";
 import RoutineBlock from "../../components/RoutineBlock/RoutineBlock";
+import emailContent from "../../utils/emailContent";
+import axios from "axios";
 
 function Results() {
   const location = useLocation();
@@ -14,6 +16,41 @@ function Results() {
   const [products, setProducts] = useState([]);
   const [routine, setRoutine] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [email, setEmail] = useState("");
+
+  // Emailing
+
+  const handleSendEmail = async () => {
+    try {
+      if (!email) {
+        alert("Entre ton email");
+        return;
+      }
+
+      if (!email.includes("@")) {
+        alert("Email invalide");
+        return;
+      }
+      // console.log("routine envoyée =", routine);
+
+      await axios.post(
+        "https://site--oh-my-skin--cvtt47qfxcv8.code.run/send-email",
+        {
+          email: email, // defini le destinataire du mail
+          //l'objet et corpss de lmail
+          subject: "Ta routine skincare est prête ✨",
+          html: emailContent(routine),
+          routine: routine,
+        },
+      );
+
+      alert("Email envoyé !");
+      console.log("Email envoyé !");
+    } catch (error) {
+      console.log(error.response?.data || error.message);
+      alert("Erreur lors de l'envoi");
+    }
+  };
 
   useEffect(() => {
     if (!answers) {
@@ -46,6 +83,19 @@ function Results() {
 
         <RoutineBlock title="Routine du Matin" products={routine.morning} />
         <RoutineBlock title="Routine du Soir" products={routine.evening} />
+
+        <div className="email-block">
+          <input
+            type="email"
+            placeholder="Entre ton email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
+
+          <button onClick={handleSendEmail}>
+            Recevoir ma routine par email
+          </button>
+        </div>
       </div>
     </main>
   );
